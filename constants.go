6354cf9f7d16630f88d269f9993042de03dc0c52
lynerist <leonardodarksoul@gmail.com>
2024-09-24 11:59:28 +0200

    start file division

---
+++ constants.go
@@ -0,0 +1,9 @@
+package main
+
+const VERBOSE = true
+const ALL 		string = "all"
+const NOT 		string = "not"
+const ANY 		string = "any"
+const ONE 		string = "one"
+const REQUIRES 	string = "requires"
+const PROVIDES 	string = "provides"
--- functions.go
+++ functions.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"crypto/md5"
 	j "encoding/json"
 	"fmt"
 	"io"
 	"os"
-	"sort"
 	"strings"
-	"github.com/ichiban/prolog"
 )
-
-const VERBOSE = true
-const ALL 		string = "all"
-const NOT 		string = "not"
-const ANY 		string = "any"
-const ONE 		string = "one"
-const REQUIRES 	string = "requires"
-const PROVIDES 	string = "provides"
 
 type artifactName	string
 type featureName	string
@@ -109,93 +98,6 @@ func log(s ...any){
 	}
 }
 
-type prologCore struct{
-	interpreter *prolog.Interpreter
-	program map[string]string
-}
-
-func (core *prologCore) addLine(s, where string){
-	core.program[where] += "\n"+s
-}
-
-func (core *prologCore)getProgram()string{
-	return fmt.Sprintf("%s\n%s\n%s\n%s\n%s\n%s", 
-						core.program["start"],
-						core.program["requiresAll"],
-						core.program["requiresNot"],
-						core.program["requiresAny"],
-						core.program["requiresOne"],
-						core.program[PROVIDES])
-}
-
-func (core prologCore) runProgram(){
-	err:=core.interpreter.Exec(core.getProgram())
-	if err != nil{
-		fmt.Println("Error in the program:", err)
-	}
-}
-
-func setupProlog() prologCore{
-	file, _ := os.Open("core.pl")
-	sc := bufio.NewScanner(file)
-	var program string
-	for sc.Scan(){
-		program += sc.Text() + "\n"
-	}
-	return prologCore{prolog.New(nil,nil), map[string]string{
-											"start":program, 
-											"requiresAll":"requiresAll(foo,foo).",
-											"requiresNot":"requiresNot(foo,foo).",
-											"requiresAny":"requiresAny(foo,foo,0).",
-											"requiresOne":"requiresOne(foo,foo,0).",
-											}}
-}
-
-var prologErrorsMeaning = map[string]string {
-	"EOF":"Missing end of the query.",
-}
-
-func prologQueryConsole(core prologCore, hashes map[string]string){
-	sc := bufio.NewScanner(os.Stdin)
-	for fmt.Print("?- "); sc.Scan(); fmt.Print("?- "){
-		query := sc.Text()
-		for hash, fullName := range hashes{
-			query = strings.ReplaceAll(query, fullName, hash)
-		}
-
-		solutions, err := core.interpreter.Query(query)
-		if err != nil{
-			if meaning, ok := prologErrorsMeaning[fmt.Sprint(err)]; ok{
-				fmt.Printf("Errore in '%s': %s\n\n", query, meaning)
-			}else{
-				fmt.Printf("Errore in '%s': %v\n\n", query, err)
-			}
-			continue
-		}
-
-		var output string
-		for solutions.Next(){
-			variables := make(map[string]any)
-			solutions.Scan(variables)
-			if len(variables) == 0{
-				output += "true."
-			}
-			toPrint := make([]string,0,len(output))
-			for k,v := range variables{
-				answer, ok := hashes["'"+fmt.Sprint(v)+"'"]
-				if !ok {answer = fmt.Sprint(v)}
-				toPrint = append(toPrint, fmt.Sprintf("%s:%s\t",k,answer))
-			}
-			sort.Strings(toPrint)
-			output += strings.Join(toPrint, "\t") + "\n"
-		}
-		
-		if output == "" {
-			fmt.Print("false.\n")
-		}
-		fmt.Println(output)
-	}
-}
 
 func insertVariables(atom any, artifact artifactName, feature featureName, variables map[artifactName]map[featureName]map[variableName]any, globals globalRegister)string{
 	stringAtom := fmt.Sprint(atom)
@@ -231,9 +133,6 @@ func (a Artifact) tags()[]any{
 
 func getArtifactsFromFeatureJSON(f any)[]any{
 	return f.(map[string]any)["artifacts"].([]any)
-}
-func getNameFromFeatureJSON(f any) featureName{
-	return featureName(f.(map[string]any)["name"].(string))
 }
 
 func stringToAN(s any)artifactName{
